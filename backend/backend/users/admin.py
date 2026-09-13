4eb4abfd5ba471e1bb957ab6e0d843dbd3e190f7
Kiran <kiranchand.0987@gmail.com>
2026-09-13 23:32:14 +0530

    chore: remove accidental duplicate backend/backend directory

--- backend/backend/users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
-
-@admin.register(User)
-class CustomUserAdmin(UserAdmin):
-    list_display = ('email', 'username', 'is_staff', 'is_superuser')
-    search_fields = ('email', 'username')
-    ordering = ('email',)
